9e443b110ee80eb3cc7f8e50cc24d47cf6b41fca
Iakovos Papadopoulos <iakopap@gmail.com>
2021-03-03 15:07:19 +0200

    Add an impl of the sequential iteration pattern

--- control-flow/sequential-execution/README.md
+++ control-flow/sequential-execution/README.md
@@ -111,6 +111,11 @@ function operation (tasks, value, callback) {
   // Launch iteration
   iterate(0);
 }
+
+// Launch the execution
+operation(tasks, value, (error, result) => {
+  ...
+});
 ```
 
 Keep in mind that we are passing information from a task to the next task by updating the current value variable with the result of each task at a time, this way we can share data between tasks. Bear in mind that we can use custom objects in the place of the value variable if we need to be more flexible according to our requirements.
@@ -120,6 +125,7 @@ Keep in mind that we are passing information from a task to the next task by upd
 Below you can find various trivial or real-world implementations of this pattern:
 
 * [text-counter](text-counter.js): a trivial example of counting chars, words and spaces in a phrase
+* [reducer](reducer.js): a trivial example of a reducer of random integer numbers
 
 ## Considerations ##
 
---
+++ control-flow/sequential-execution/reducer.js
@@ -0,0 +1,51 @@
+const rand = (max) => {
+  return (cb) => {
+    setTimeout(() => {
+      try {
+        const number = Math.floor(Math.random() * max) + 1;
+
+        cb(null, number);
+      } catch (error) {
+        cb(error);
+      }
+    });
+  }
+}
+
+function reduce (tasks, cb) {
+  const context = { numbers: [], value: 0 };
+
+  function iterate (index) {
+    if (index === tasks.length) {
+      return cb(null, context);
+    }
+
+    const task = tasks[index];
+    
+    task((error, result) => {
+      if (error) {
+        return cb(error);
+      }
+
+      context.numbers.push(result);
+      context.value += result;
+
+      iterate(index + 1);
+    });
+  }
+
+  iterate(0);
+}
+
+const tasks = [rand(2), rand(4), rand(6)];
+
+reduce(tasks, (error, result) => {
+  if (error) {
+    return console.error(error);
+  }
+
+  console.log(result);
+});
+
+// Async output:
+// { numbers: [ 1, 4, 3 ], value: 8 }
